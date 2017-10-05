3bcaf14d176026ffa572b454d048d0c6e3be5c8d
Patrick Smith <patrick@burntcaramel.com>
2017-10-05 15:05:42 +1100

    Fix handling of validation when just displaying the form

--- app/controllers/number_converter_controller.rb
+++ app/controllers/number_converter_controller.rb
@@ -2,7 +2,7 @@ class NumberConverterController < ApplicationController
   def index
     safe_params = params.permit(:number, :base)
     @number_conversion = NumberConversion.new(safe_params)
-    @number_conversion.save
+    @number_conversion.save if request.post?
 
     @past_conversions = NumberConversion.all.order(created_at: :desc)
   end
